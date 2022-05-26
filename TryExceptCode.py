f52fad5e91d7bbe886ded6eab69c284d60c579e0
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2022-05-26 18:01:43 +0530

    Exception Handling
    Exception Handling - The Python Series #5

---
+++ TryExceptCode.py
@@ -0,0 +1,8 @@
+try:
+  print (variableName)
+except: 
+  print("Something is fishy! Oh! variableName is not declared")
+else: 
+  print("Everything looks perfect!")
+finally:
+  print("This block is working like a charm HURRY🎉")
---
+++ TryExceptCode02.py
@@ -0,0 +1,9 @@
+variableName = 34
+try:
+  print (variableName)
+except: 
+  print("Something is fishy! Oh! variableName is not declared")
+else: 
+  print("Everything looks perfect!")
+finally:
+  print("This block is working like a charm HURRY🎉")
